94890189128e980562c1d975a890636906dae0ef
Bassem <BasemOmar2012@gmail.com>
2024-12-01 18:46:57 +0200

    remove unused imports .

---
+++ app/src/main/java/com/bassem/demo_plants/presentation/ui/home/HomeList.kt
@@ -0,0 +1,38 @@
+package com.bassem.demo_plants.presentation.ui.home
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.bassem.demo_plants.R
+import com.bassem.demo_plants.data.models.Data
+
+@Preview(showBackground = true)
+@Composable
+fun HomeListPreview() {
+    HomeList(propertiesList = listOf(), onClick = {})
+}
+
+@Composable
+fun HomeList(
+    propertiesList: List<Data>,
+    onClick: (Data) -> Unit,
+) {
+    LazyColumn (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = dimensionResource(id = R.dimen.default_padding))
+    ) {
+        items(propertiesList, key = { it.id }) { item ->
+            PlantItemView(
+                item,
+                onCardClick = { onClick(item) },
+            )
+        }
+    }
+
+}
---
+++ app/src/main/java/com/bassem/demo_plants/presentation/ui/home/HomeScreen.kt
@@ -0,0 +1,38 @@
+package com.bassem.demo_plants.presentation.ui.home
+
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.layout.AnimatedPane
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
+import androidx.compose.material3.adaptive.navigation.NavigableListDetailPaneScaffold
+import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.bassem.demo_plants.data.models.Data
+import com.bassem.demo_plants.presentation.ui.details.DetailsScreen
+
+@OptIn(ExperimentalMaterial3AdaptiveApi::class)
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    val navigator = rememberListDetailPaneScaffoldNavigator<Any>()
+
+    NavigableListDetailPaneScaffold(
+        navigator = navigator,
+        listPane = {
+            AnimatedPane {
+                PropertiesListScreen(modifier = modifier, onClick = { plant ->
+                    navigator.navigateTo(
+                        pane = ListDetailPaneScaffoldRole.Detail,
+                        content = plant
+                    )
+                })
+            }
+        },
+        detailPane = {
+            AnimatedPane {
+                navigator.currentDestination?.content?.let { plant ->
+                    DetailsScreen(plant = plant as Data, modifier)
+                }
+            }
+        },
+    )
+}
