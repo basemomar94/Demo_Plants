9d6e96f07d20f0eb7065699320f18f133282bbc2
Bassem <BasemOmar2012@gmail.com>
2024-12-01 15:28:26 +0200

    create plant text compose

---
+++ app/src/main/java/com/bassem/demo_plants/presentation/ui/home/PlantName.kt
@@ -0,0 +1,39 @@
+package com.bassem.demo_plants.presentation.ui.home
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.bassem.demo_plants.R
+
+
+@Preview
+@Composable
+private fun PlantNamePreview() {
+    PlantName("Rose")
+}
+
+@Composable
+fun PlantName(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = name,
+        style = MaterialTheme.typography.titleMedium.copy(
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary,
+            textAlign = TextAlign.Center
+        ),
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.default_padding))
+    )
+}
+
