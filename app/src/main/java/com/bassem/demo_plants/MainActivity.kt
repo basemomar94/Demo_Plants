128bf3a3510a942c5437735f19107d9cf3f9c0b7
Bassem <BasemOmar2012@gmail.com>
2024-12-01 18:17:50 +0200

    remove unused imports

--- app/src/main/java/com/bassem/demo_plants/MainActivity.kt
+++ app/src/main/java/com/bassem/demo_plants/MainActivity.kt
@@ -7,12 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.bassem.demo_plants.presentation.ui.home.HomeScreen
 import com.bassem.demo_plants.ui.theme.Demo_PlantsTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Demo_PlantsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text(text = "Plants", modifier = Modifier.padding(innerPadding))
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
