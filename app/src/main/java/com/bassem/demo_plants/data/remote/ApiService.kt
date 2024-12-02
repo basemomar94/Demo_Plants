b4d2a93ec1b204e7b424639ac2df2242fe4df145
Bassem <BasemOmar2012@gmail.com>
2024-12-02 12:30:52 +0200

    add a comment for deprecated field

--- app/src/main/java/com/bassem/demo_plants/data/remote/ApiService.kt
+++ app/src/main/java/com/bassem/demo_plants/data/remote/ApiService.kt
@@ -4,6 +4,13 @@ import com.bassem.demo_plants.data.models.PlantsResult
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+/**
+ * When I select a zone filter from the available filters (All, Palestine, Sudan,
+ * Myanmar,Transcaucasus, Uzbekistan), only plants with the selected filter must
+ * be shown. this was asked as a part of the task, but the distribution field was deprecated according to the api
+ * documentation https://docs.trefle.io/docs/advanced/plants-fields/
+ */
+
 interface ApiService {
     @GET("plants")
     suspend fun getPlants(
