1ede67bddc379b73830162e7d569fc2c1d6df51c
Bassem <BasemOmar2012@gmail.com>
2024-12-02 13:58:24 +0200

    change minSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.bassem.demo_plants"
-        minSdk = 28
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
