767954736255db4c64dc6ccc14fa30f2b0566e83
Bassem <BasemOmar2012@gmail.com>
2024-12-02 13:46:15 +0200

    remove un-used imports

--- app/src/main/java/com/bassem/demo_plants/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/bassem/demo_plants/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.bassem.demo_plants.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
